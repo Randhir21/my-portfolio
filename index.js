c02e898ddf4842aa6f938ef9c5d921107f2610e6
Randhir21 <rps85071@gmail.com>
2024-03-05 23:01:11 +0530

    Optimization

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-let menu=document.getElementById('menu');
-let menu_open=document.getElementById('menu_open');
-let menu_close=document.getElementById('menu_close');
-
-function toggleMenu(){
-    
-    menu.style.display='block';
-    menu_close.style.display='block';
-}
-function toggleMenuClose(){
-    if (screen.width <= 920) {
-        menu.style.display='none';
-        menu_close.style.display='none';
-    }
-}
-
-
-// let windowSize = document.write(window);
-// console.log(windowSize);
-function toggle(){
-if (screen.width <= 920) {
-    menu.style.display='none';
-    menu_close.style.display='none';
-} 
-}
+let menu=document.getElementById('menu');let menu_open=document.getElementById('menu_open');let menu_close=document.getElementById('menu_close');function toggleMenu(){menu.style.display='block';menu_close.style.display='block'}
+function toggleMenuClose(){if(screen.width<=920){menu.style.display='none';menu_close.style.display='none'}}
+function toggle(){if(screen.width<=920){menu.style.display='none';menu_close.style.display='none'}}
